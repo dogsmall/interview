71a36bc3a9e045da82a9d2f8f981d6775a3c91ee
guoshiwei <guoshiwei@tal.com>
2021-08-11 17:11:21 +0800

    feat:数据结构-栈

---
+++ Stack/balanced-symbols.js
@@ -0,0 +1,74 @@
+class Stack {
+  constructor() {
+    this.count = 0;
+    this.items = {};
+  }
+  push(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.count - 1];
+  }
+  isEmpty() {
+    return this.count === 0;
+  }
+  size() {
+    return this.count;
+  }
+  clear() {
+    this.items = {};
+    this.count = 0;
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    let objString = `${this.items[0]}`;
+    for (let i = 1; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`;
+    }
+    return objString;
+  }
+}
+console.log("{([])}", parenthesesChecker("{([])}")); // true
+console.log("{{([][])}()}", parenthesesChecker("{{([][])}()}")); // true
+console.log("[{()]", parenthesesChecker("[{()]")); // false
+
+function parenthesesChecker(symbols) {
+  const stack = new Stack();
+  const opens = '([{';
+  const closers = ')]}';
+  let balanced = true;
+  let index = 0;
+  let symbol;
+  let top;
+
+  while (index < symbols.length && balanced) {
+    symbol = symbols[index];
+    if (opens.indexOf(symbol) >= 0) {
+      stack.push(symbol);
+    } else if (stack.isEmpty()) {
+      balanced = false;
+    } else {
+      top = stack.pop();
+      if (!(opens.indexOf(top) === closers.indexOf(symbol))) {
+        balanced = false;
+      }
+    }
+    index++;
+  }
+  return balanced && stack.isEmpty();
+}
